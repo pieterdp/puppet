51bdbd75ffe75d772448e6fb3d32800ab61be297
Pieter De Praetere <pieter.de.praetere@helptux.be>
2016-10-11 20:31:46 +0200

    Bugfix

--- pp_backup/manifests/config/user.pp
+++ pp_backup/manifests/config/user.pp
@@ -2,7 +2,7 @@
 # Configure the backup user
 class pp_backup::config::user () inherits pp_backup {
 
-    include pp_accunts
+    include pp_accounts
 
     pp_accounts::acount {'rdiff-backup':
         authorized_keys => $pp_backup::backup_user_auth_keys,

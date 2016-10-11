43b63ab81cc5f0094d0fabbf6c7b6f1d54131631
Pieter De Praetere <pieter.de.praetere@helptux.be>
2016-10-11 20:43:01 +0200

    Removed mysql_backup.sh (duplicate declaration)

--- pp_backup/manifests/install/scripts.pp
+++ pp_backup/manifests/install/scripts.pp
@@ -9,7 +9,7 @@ class pp_backup::install::scripts () inherits pp_backup {
         mode   => '0770',
     }
 
-    each(['external_backup.sh', 'mysql_backup.sh', 'run_script']) | $script | {
+    each(['external_backup.sh', 'run_script']) | $script | {
         file {"/opt/backup/${script}":
             source => "puppet:///modules/pp_backup/${script}",
         }
